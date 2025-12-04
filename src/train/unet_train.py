be0415eefaa9fc6b8343db27f6577ad7431a25ad
Yoann Lafore <yoann.lafore@gmail.com>
2025-12-04 22:20:23 +0100

    feat(train): add train epoch for improved model

--- src/train/unet_train.py
+++ src/train/unet_train.py
@@ -7,7 +7,7 @@ import wandb
 from src.utils import *
 import json
 
-from src.train.train_utils import evaluate_epoch, train_epoch
+from src.train.train_utils import *
 
 # TODO :
 # - Why are the validation metrics doing zigzag ?
@@ -134,6 +134,187 @@ def train_model_on_ds(train_ds, test_ds, cfg):
     torch.save(model.state_dict(), final_model_path)
 
     print(f"Saved final model: {final_model_path}")
+
+    return final_stats
+
+
+def train_improve_model_on_ds(train_ds, test_ds, cfg):
+    print(f"Using out dir: {cfg.train.out_dir}")
+
+    train_cfg = cfg.train
+    device = cfg.device
+
+    base_model_cfg = cfg.base_model
+    base_model = build_from_cfg(base_model_cfg).to(device)
+
+    improved_model_cfg = cfg.improvement_model
+    improved_model = build_from_cfg(improved_model_cfg).to(device)
+
+    collate_fn = build_from_cfg(cfg.train.collate_fn)
+    batch_size = int(train_cfg.batch_size)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=collate_fn,
+        pin_memory=True,
+        num_workers=4,
+    )
+
+    val_loader = torch.utils.data.DataLoader(
+        test_ds,
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=collate_fn,
+        pin_memory=True,
+        num_workers=4,
+    )
+
+    train_jointly = bool(train_cfg.train_jointly)
+
+    # Make sure output directory exists
+    out_dir = train_cfg.out_dir
+    checkpoint_dir = os.path.join(out_dir, "checkpoints/")
+    validation_file = os.path.join(out_dir, "validation.json")
+    final_stats_file = os.path.join(out_dir, "final_stats.json")
+    os.makedirs(out_dir, exist_ok=True)
+    os.makedirs(checkpoint_dir, exist_ok=True)
+
+    # Save config file
+    dump_cfg(cfg, os.path.join(out_dir, "config.yaml"))
+
+    log_wandb = bool(train_cfg.log_wandb)
+    nb_epochs = int(train_cfg.epochs)
+    validate_every = int(train_cfg.validate_every)
+    checkpoint_every = int(train_cfg.checkpoint_every)
+
+    if train_jointly:
+        params = list(base_model.parameters()) + list(improved_model.parameters())
+        optimizer = build_from_cfg(train_cfg.optimizer, params=params)
+        criterion = build_from_cfg(train_cfg.criterion)
+        lr_scheduler = build_from_cfg(train_cfg.lr_scheduler, optimizer=optimizer)
+    else:
+        base_optimizer = build_from_cfg(
+            train_cfg.base_optimizer, params=base_model.parameters()
+        )
+        improved_optimizer = build_from_cfg(
+            train_cfg.improved_optimizer, params=improved_model.parameters()
+        )
+        base_criterion = build_from_cfg(train_cfg.base_criterion)
+        improved_criterion = build_from_cfg(train_cfg.improved_criterion)
+        base_lr_scheduler = build_from_cfg(
+            train_cfg.base_lr_scheduler, optimizer=base_optimizer
+        )
+        improved_lr_scheduler = build_from_cfg(
+            train_cfg.improved_lr_scheduler, optimizer=improved_optimizer
+        )
+
+    if log_wandb:
+        wandb.init(
+            project="road-segmentation",
+            config=OmegaConf.to_container(cfg, resolve=True),
+        )
+
+    progress = tqdm(range(nb_epochs), desc="Training Epochs")
+
+    for epoch in progress:
+        if train_jointly:
+            train_loss = train_epoch_improve_module_joint(
+                base_model,
+                improved_model,
+                optimizer,
+                train_loader,
+                criterion,
+                device,
+                log_wandb=log_wandb,
+            )
+            if isinstance(lr_scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                lr_scheduler.step(train_loss)
+            else:
+                lr_scheduler.step()
+            progress.set_postfix({"train_loss": f"{train_loss:.4f}"})
+        else:
+            train_loss_base, train_loss_improved = train_epoch_improve_module_sep(
+                base_model,
+                improved_model,
+                base_optimizer,
+                improved_optimizer,
+                train_loader,
+                base_criterion,
+                improved_criterion,
+                device,
+                log_wandb=log_wandb,
+            )
+            if isinstance(
+                base_lr_scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau
+            ):
+                base_lr_scheduler.step(train_loss_base)
+            else:
+                base_lr_scheduler.step()
+            if isinstance(
+                improved_lr_scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau
+            ):
+                improved_lr_scheduler.step(train_loss_improved)
+            else:
+                improved_lr_scheduler.step()
+            progress.set_postfix(
+                {
+                    "train_loss": f"{train_loss_improved:.4f}",
+                }
+            )
+
+        if log_wandb:
+            wandb.log({"learning_rate": optimizer.param_groups[0]["lr"]})
+
+        if (epoch + 1) % validate_every == 0:
+            val_stats_base, val_stats_improved = evaluate_epoch_improve_module(
+                base_model,
+                improved_model,
+                val_loader,
+                criterion,
+                device,
+                log_wandb=log_wandb,
+            )
+
+            val_stats_improved["epoch"] = epoch + 1
+            # Save validation stats
+            append_val_json(validation_file, val_stats_improved)
+
+        if (epoch + 1) % checkpoint_every == 0:
+            base_checkpoint_path = os.path.join(
+                checkpoint_dir, f"base_unet_epoch_{epoch+1}.pth"
+            )
+            improved_checkpoint_path = os.path.join(
+                checkpoint_dir, f"improved_unet_epoch_{epoch+1}.pth"
+            )
+            torch.save(base_model.state_dict(), base_checkpoint_path)
+            torch.save(improved_model.state_dict(), improved_checkpoint_path)
+
+    # Evaluate one last time on validation set
+    _, final_stats = evaluate_epoch_improve_module(
+        base_model,
+        improved_model,
+        val_loader,
+        criterion,
+        device,
+        log_wandb=log_wandb,
+    )
+    # Save final stats
+    json.dump(final_stats, open(final_stats_file, "w"), indent=4)
+
+    if log_wandb:
+        wandb.finish()
+
+    # Save final model
+    base_final_model_path = os.path.join(out_dir, "base_final_model.pth")
+    torch.save(base_model.state_dict(), base_final_model_path)
+
+    improved_final_model_path = os.path.join(out_dir, "improved_final_model.pth")
+    torch.save(improved_model.state_dict(), improved_final_model_path)
+
+    print(f"Saved base final model: {base_final_model_path}")
+    print(f"Saved improved final model: {improved_final_model_path}")
 
     return final_stats
 
